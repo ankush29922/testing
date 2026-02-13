39b93ceeab4cdf10a8404d554378a5285742e8a5
xen <xen2992on@gmail.com>
2026-02-13 22:00:22 +0530

    Added +18 functions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 
-const name = 'xenos'
+const name1 = 'xenos'
 const dob = '01/02/2004'
 
 function sayName(){
-    console.log('My name is ' + name)
+    console.log('My name is ' + name1)
+}
+
+function IsAbove18() {
+    return age >= 18;
 }
