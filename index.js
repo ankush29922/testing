6470aeb7f0d51e5e892db2b4bf920de717952aca
xen <xen2992on@gmail.com>
2026-02-13 22:28:28 +0530

    Revert "Added +18 functions"
    This reverts commit 39b93ceeab4cdf10a8404d554378a5285742e8a5.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 
 
-const name1 = 'xenos'
+const name = 'xenos'
 const dob = '01/02/2004'
 
 function sayName(){
-    console.log('My name is ' + name1)
-}
-
-function IsAbove18() {
-    return age >= 18;
+    console.log('My name is ' + name)
 }
